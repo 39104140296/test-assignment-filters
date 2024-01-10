efa10be487542cec715381091db9bf9b50cd1bb7
August Vaiksoo <august.vaiksoo@posteo.net>
2024-01-10 12:42:24 +0200

    Implement all the methods in apiService for application functionalities

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -11,7 +11,37 @@ export const getAllFiltersWithCriteria = async () => {
     const response = await axiosInstance.get('/filters/with-criteria')
     return response.data
   } catch (error) {
-    console.error('Error fetching filters with criteria:', error)
+    console.error('Error fetching filters with criteria: ', error)
+    throw error
+  }
+}
+
+export const createFilter = async (filter) => {
+  try {
+    const response = await axiosInstance.post('/filters/', filter)
+    return response.data
+  } catch (error) {
+    console.error('Error creating filter: ', error)
+    throw error
+  }
+}
+
+export const updateFilter = async (filterId, filter) => {
+  try {
+    const response = await axiosInstance.put(`/filters/${filterId}`, filter)
+    return response.data
+  } catch (error) {
+    console.error('Error updating filter: ', error)
+    throw error
+  }
+}
+
+export const deleteFilter = async (filterId) => {
+  try {
+    const response = await axiosInstance.delete(`/filters/${filterId}`)
+    return response.data
+  } catch (error) {
+    console.error('Error deleting filter: ', error)
     throw error
   }
 }
