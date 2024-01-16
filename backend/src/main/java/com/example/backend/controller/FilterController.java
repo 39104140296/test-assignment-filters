3234e8099aa6334c1b519af3bd3fa25e11ef0df0
August Vaiksoo <august.vaiksoo@posteo.net>
2024-01-16 23:29:22 +0200

    remove wildcard import

--- backend/src/main/java/com/example/backend/controller/FilterController.java
+++ backend/src/main/java/com/example/backend/controller/FilterController.java
@@ -1,6 +1,7 @@
 package com.example.backend.controller;
 
 import com.example.backend.service.FilterService;
+
 import com.example.backend.dto.FilterDTO;
 import com.example.backend.dto.FilterCriteriaDTO;
 import com.example.backend.controller.request.CreateFilterRequest;
@@ -11,7 +12,14 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/filters")
