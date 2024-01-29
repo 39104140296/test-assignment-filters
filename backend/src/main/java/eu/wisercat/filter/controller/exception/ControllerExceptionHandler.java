9b61421c262ce2733340e2267d761e55f8b4c23b
August Vaiksoo <august.vaiksoo@posteo.net>
2024-01-29 23:06:20 +0200

    Refactor exception handler and fix typo for logging in application.properties

--- backend/src/main/java/eu/wisercat/filter/controller/exception/ControllerExceptionHandler.java
+++ backend/src/main/java/eu/wisercat/filter/controller/exception/ControllerExceptionHandler.java
@@ -6,40 +6,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.util.WebUtils;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public final ResponseEntity<ApiException> handleException(Exception ex, WebRequest request) {
-        final HttpHeaders headers = new HttpHeaders();
-        if (ex instanceof EntityNotFoundException exception) {
-            final HttpStatus status = HttpStatus.NOT_FOUND;
-            return handleEntityNotFoundException(exception, headers, status, request);
-        } else {
-            final HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-            return handleInternalException(ex, null, headers, status, request);
-        }
+    public final ResponseEntity<ApiException> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return handleException(ex, NOT_FOUND);
     }
 
-    protected ResponseEntity<ApiException> handleEntityNotFoundException(EntityNotFoundException ex,
-                                                                         HttpHeaders headers,
-                                                                         HttpStatus status,
-                                                                         WebRequest request) {
-        return handleInternalException(ex, new ApiException(ex.getMessage()), headers, status, request);
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ApiException> handleInternalException(Exception ex) {
+        return handleException(ex, INTERNAL_SERVER_ERROR);
     }
 
-    protected ResponseEntity<ApiException> handleInternalException(Exception ex,
-                                                                   ApiException body,
-                                                                   HttpHeaders headers,
-                                                                   HttpStatus status,
-                                                                   WebRequest request) {
-        if (HttpStatus.INTERNAL_SERVER_ERROR.equals(status)) {
-            request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, WebRequest.SCOPE_REQUEST);
-        }
-
-        return new ResponseEntity<>(body, headers, status);
+    private ResponseEntity<ApiException> handleException(Exception ex, HttpStatus status) {
+        return new ResponseEntity<>(new ApiException(ex.getLocalizedMessage()), new HttpHeaders(), status);
     }
 }
